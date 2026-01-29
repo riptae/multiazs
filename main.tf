467810523fb9d83b6fb02504ea6ccf6fa986a225
yun <riptaggart@naver.com>
2026-01-29 20:58:22 +0900

    DB용 private subnet 구조로 변경

--- main.tf
+++ main.tf
@@ -24,6 +24,9 @@ resource "aws_vpc" "main" {
   cidr_block           = "10.7.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+  tags = {
+    Name = "vpc_multiazs"
+  }
 }
 
 # [2] subnets
@@ -32,36 +35,90 @@ resource "aws_subnet" "public_a" {
   availability_zone       = "ap-northeast-2a"
   cidr_block              = "10.7.1.0/24"
   map_public_ip_on_launch = true
+  tags = {
+    Name = "public_subnet_a"
+  }
 }
 
-resource "aws_subnet" "public_b" {
+resource "aws_subnet" "private_a" {
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = "ap-northeast-2a"
+  cidr_block              = "10.7.2.0/24"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "private_subnet_a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
   vpc_id                  = aws_vpc.main.id
   availability_zone       = "ap-northeast-2b"
-  cidr_block              = "10.7.2.0/24"
-  map_public_ip_on_launch = true
+  cidr_block              = "10.7.3.0/24"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "private_subnet_b"
+  }
 }
 
-# [3] igw
+# [3] igw / NAT gw
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
+resource "aws_eip" "eip" {
+  domain = "vpc"
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "natgw" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public_a.id
+
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "main-nat-gw"
+  }
+}
+
 # [4] route table
-resource "aws_route_table" "rt" {
+resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgw.id
+  }
+  tags = {
+    Name = "private-rt"
   }
 }
 
 # [5] assoc
 resource "aws_route_table_association" "assoc_a" {
   subnet_id      = aws_subnet.public_a.id
-  route_table_id = aws_route_table.rt.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_route_table_association" "assoc_b" {
-  subnet_id      = aws_subnet.public_b.id
-  route_table_id = aws_route_table.rt.id
+resource "aws_route_table_association" "private_assoc_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_rt.id
 }
+
+resource "aws_route_table_association" "private_assoc_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
